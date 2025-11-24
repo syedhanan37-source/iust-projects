bd3ad49b02574644fa430ba345cbd7288d7b2c4d
azimhussaindar-max <azimhussaindar@gmail.com>
2025-11-25 01:07:03 +0530

    Update even-odd.c

--- even-odd.c
+++ even-odd.c
@@ -2,4 +2,17 @@
 
 int main()
 {
-  printf
+   int a;
+   printf("enter first number: ");
+   scanf("%d" , &a);
+
+  if (a % 2 == 0)
+  {
+  printf("%d is an even number\n", a);
+  }
+  else 
+  {
+    printf("%d is an odd number\n", a);
+  return 0;
+}
+
